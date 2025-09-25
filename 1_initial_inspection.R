a9446ce9ccef059ac47df74025a5745747b812f3
Sierra Scott <sierrascott2026@u.northwestern.edu>
2025-09-25 07:53:06 -0400

    set up

---
+++ 1_initial_inspection.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(tidymodels)
+library(here)
+library(naniar)
+
+persons_contracts <- read_csv(here("data/voc_persons_contracts.csv"))
+ranks <-  read_csv(here("data/voc_ranks.csv"))
+
+miss_var_summary(persons_contracts) |>
+print(n = 27)
+
+miss_var_summary(ranks) |>
+print(n = 11)
+
+persons_contracts %>%
+  distinct(reason_end_contract) |>
+  print(n = 22)
+
+persons_contracts %>%
+  distinct(rank) 
+  
